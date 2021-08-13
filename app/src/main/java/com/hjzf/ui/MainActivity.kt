8b0da84baac654087d306fb121041598c33e59b9
hjzf <1187093389@qq.com>
2021-08-13 16:38:58 +0800

    refactor: use view binding

--- app/src/main/java/com/hjzf/ui/MainActivity.kt
+++ app/src/main/java/com/hjzf/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hjzf.R
 import com.hjzf.databinding.ActivityMainBinding
 import com.hjzf.util.LogUtil
@@ -15,30 +14,32 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var currentNavController: NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         LogUtil.e(TAG, "onCreate: ")
         super.onCreate(savedInstanceState)
-        // View binding
+        // 设置 View binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // Init "currentNavController"
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_container
-        ) as NavHostFragment
-        currentNavController = navHostFragment.navController
+        // 在xml中为 nav_host_container 添加两个属性 :
+        // android:name="androidx.navigation.fragment.NavHostFragment"
+        // app:navGraph="@navigation/nav_graph"
 
-        // Setup the bottom navigation view with navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
-        bottomNavigationView.setupWithNavController(currentNavController)
+        // (1)初始化 navController,
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // (2)关联 “底部导航栏”与“navController”, 导航路由见 app/src/main/res/navigation/nav_graph.xml
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         LogUtil.e(TAG, "onSupportNavigateUp: ")
-        return currentNavController.navigateUp()
+        return navController.navigateUp()
     }
 
     companion object {
